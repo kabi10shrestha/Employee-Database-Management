72e11c06f5de7d6cc486c10b9b2f885d161a173c
kabi10shrestha <kabi10shrestha@yahoo.com>
2020-04-27 02:20:06 +0700

    Updated index.php
    Tables fixed, option select added, date input missing

--- index.php
+++ index.php
@@ -13,6 +13,27 @@
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" rel="stylesheet" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/dataTables.bootstrap4.min.js"></script>
+<script> type="text/javascript"
+  $(document).ready(function() {
+    $('#example').DataTable();
+  });
+</script>
+
+
+ <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker3').datetimepicker({
+                    format: 'LT'
+                });
+            });
+        </script>
+
+
+
+
 </head>
 <body>
 
@@ -23,7 +44,6 @@
       <a class="navbar-brand" href="index.php">Employee Database</a>
     </div>
     <ul class="nav navbar-nav">
-      <li><a href="index.php">Home</a></li>
       <li><a href="index.php?page=employee">Employees</a></li>
       <li><a href="index.php?page=department">Departments</a></li>
 	  <li><a href="index.php?page=projects">Projects</a></li>
@@ -32,6 +52,8 @@
   </div>
 </nav>
 
+
+
 <?php
 
 
@@ -39,8 +61,8 @@ function employee_page() {
 	require('connect.php');
 	$result = $conn->query("SELECT * FROM employee LEFT JOIN department ON department.dnumber = employee.dno");
 	//WHERE not exists (SELECT * FROM  dependent WHERE ssn=essn) ORDER BY dname ASC, lname ASC;";
-	echo "<div class='container'><h2>Employee Database</h2><table id='example' class='table table-striped table-inverse table-bordered table-hover' cellspacing='0' width='100%'>";
-    echo "<tr><th>First</th><th>Last</th><th>SSN</th><th>DOB</th><th>Address</th><th>Sex</th><th>Salary</th><th>SuperSSN</th><th>Department</th></tr>";
+	echo "<div class='container'><h2>Employee Database</h2><table id='example' class='table table-striped table-inverse table-bordered table-hover' cellspacing='0' width='100%' >";
+    echo "<thead><tr><th>First</th><th>Last</th><th>SSN</th><th>DOB</th><th>Address</th><th>Sex</th><th>Salary</th><th>SuperSSN</th><th>Department</th></tr></thead><tbody>";
 	while($row = $result->fetch_assoc()){
 	 echo "<tr>";
 	 echo "<td>".$row["fname"]."</td>";
@@ -54,61 +76,60 @@ function employee_page() {
 	 echo "<td>".$row["dname"]."</td>";    
 	 echo "</tr>";
 	}
-	 echo '</table></div>';
+	 echo '</tbody></table>';
 	 
 	 
+$manager_result = $conn->query("SELECT mgrssn, fname, lname FROM employee, department where mgrssn = ssn");
+$department_result = $conn->query("SELECT dnumber, dname FROM department");
 
 
-
- if (isset($_POST['first_name'])) {
+ if (isset($_POST['submit'])) {
   print("Employee Added, ");
   //we should call $u = $conn->mysqli::real_escape_string()
   //if we have a database connection
   $fname = $_POST['first_name'];
   $lname = $_POST['last_name'];
   $ssn = $_POST['ssn'];
+  /*
   $dob = $_POST['dob'];
   $address = $_POST['address'];
   $sex = $_POST['sex'];
   $salary = $_POST['salary'];
-  $manager = $_POST['manager'];
-  $dept = $_POST['dept_name'];
-  print($lname.", ".$fname.", ".$ssn);
+  $manager = $_POST['mgr'];
+  $dept = $_POST['dept'];
+  */
+  print($fname." ".$lname.", ".$ssn);
 }
 else {
-	echo "<p>";
-
+	echo "<p><table>";
   print("<form method=\"post\" action=\"$_SERVER[PHP_SELF]\">");
   echo "<input type='hidden' name='page' value='addemployee'/>";
-  print("First Name: <input type=\"text\" name=\"first_name\" required>");
-  echo "<br />";
-  print("Last Name: <input type=\"text\" name=\"last_name\" required>");
-  echo "<br />";
-  print("SSN: <input type=\"number\" name=\"ssn\" required>");
-  echo "<br />";
-  print("DOB: <input type=\"text\" name=\"dob\" required>");
-  echo "<br />";
-  print("Address: <input type=\"text\" name=\"address\" required>");
-  echo "<br />";
-  print("Sex: <input type=\"text\" name=\"sex\" required>");
-  echo "<br />";
-  print("Salary: <input type=\"number\" name=\"salary\" required>");
-  echo "<br />";
-  print("Manager: <input type=\"number\" name=\"manager\" required>");
-  /*<label for="mgr">Managers:</label>
-<select id="mgr">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="vw">VW</option>
-  <option value="audi" selected>Audi</option>
-</select>
-*/
-  echo "<br />"; 
-  print("Department: <input type=\"number\" name=\"dept_name\" required>");
-  echo "<br />";
-  print("<input type=\"submit\" value=\"Add Employee\">");
+  print("<tr><th>First Name: </th><th><input type=\"text\" name=\"first_name\" maxlength=\"10\" required></th></tr>");
+  print("<tr><th>Last Name: </th><th><input type=\"text\" name=\"last_name\" maxlength=\"20\" required></th></tr>");
+  print("<tr><th>SSN: </th><th><input type=\"number\" name=\"ssn\" max=\"999999999\" required></th></tr>");
+  print("<tr><th>DOB: </th><th><input type=\"date\" name=\"dob\" required></th></tr>");
+  print("<tr><th>Address: </th><th><input type=\"text\" name=\"address\" maxlength=\"30\" required></th></tr>");
+  print("<tr><th>Sex: </th><th><input type=\"text\" name=\"sex\" maxlength=\"1\" required></th></tr>");
+  print("<tr><th>Salary: </th><th><input type=\"number\" name=\"salary\" max=\"99999\" required></th></tr>");
+  
+  print("<tr><th><label for=\"mgr\">Manager:</label></th><th><select id=\"mgr\" name=\"mgr\">");
+  echo "<option disabled selected value> -- select a manager -- </option>";
+  while($row = $manager_result->fetch_assoc()){
+  echo "<option value=".$row["mgrssn"].">".$row["fname"]." ".$row["lname"]."</option>";
+  }
+  echo "</select></th></tr>";
+  
+  print("<tr><th><label for=\"dept\">Department:</label></th><th><select id=\"dept\" name=\"dept\" required>");
+  echo "<option disabled selected value> -- select a department -- </option>";
+  while($row = $department_result->fetch_assoc()){
+  echo "<option value=".$row["dnumber"].">".$row["dname"]."</option>";
+  }
+  echo "</select></th></tr>";
+  
+  echo "<br /></table>";
+  print("<input type=\"submit\" name=\"submit\" value=\"Add Employee\">");
   print("</form>");
-  echo "<p>";
+  echo "</p></div>";
 }
 $conn->close();
 }
@@ -135,11 +156,11 @@ function add_employee() {
 		 $address = $_POST['address'];
 		 $sex = $_POST['sex'];
 		 $salary = $_POST['salary'];
-		 $manager = $_POST['manager'];
-	     $dept = $_POST['dept_name'];        
-         $stmt->bind_param("ssiissiii", $fname, $lname, $ssn, $dob, $address, $sex, $salary, $manager, $dept);
+		 $manager = $_POST['mgr'];
+	     $dept = $_POST['dept'];        
+         $stmt->bind_param("ssidssiii", $fname, $lname, $ssn, $dob, $address, $sex, $salary, $manager, $dept);
          if ($stmt->execute()) {
-              echo "ok <a href='employee_table.php'>Back</a>";
+              echo "ok <a href='index.php?page=employee'>Back</a>";
          } else 
          {
               echo "Fail";
@@ -152,16 +173,12 @@ function add_employee() {
 
 
 function department_page() {
-	echo "<div class='container'><h2>Departments</h2><table id='example' class='table table-striped table-inverse table-bordered table-hover' cellspacing='0' width='100%'>";
-	echo "<th>Department Name</th><th>Number</th><th>Manager</th>";
-    echo "<tbody>";
-	
 	require('connect.php');
-
 $sql = "SELECT * from department LEFT JOIN employee ON employee.ssn = department.mgrssn";
 $result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
+	
+	echo "<div class='container'><h2>Departments</h2><table id='example' class='table table-striped table-inverse table-bordered table-hover' cellspacing='0' width='100%' >";
+	echo "<thead><tr><th>Department Name</th><th>Number</th><th>Manager</th></tr></thead><tbody>";
 	while($row = $result->fetch_assoc()){
 	 echo "<tr>";
 	 echo "<td>".$row["dname"]."</td>";  
@@ -169,15 +186,12 @@ if ($result->num_rows > 0) {
 	  echo "<td>".$row["fname"]."</td>"; 
 	 echo "</tr>";
 	}
-} else {
-	echo "0 results";
-}
-$conn->close();
+	echo '</tbody></table>';
 
 
-    echo '</tbody></table></div>';
+    $dept_manager_result = $conn->query("SELECT ssn, fname, lname FROM employee");
 
-if (isset($_POST['dept_name'])) {
+if (isset($_POST['submit2'])) {
   print("Department Added, ");
   //we should call $u = $conn->mysqli::real_escape_string()
   //if we have a database connection
@@ -185,20 +199,33 @@ if (isset($_POST['dept_name'])) {
   print($u);
 }
 else {
+	echo "<p><table>";
   print("<form method=\"post\" action=\"$_SERVER[PHP_SELF]\">");
   echo "<input type='hidden' name='page' value='adddepartment'/>";
-  print("Add a new department: <input type=\"text\" name=\"dept_name\" required>");
-  echo "<br />";
-  print("Department Number: <input type=\"text\" name=\"dept_num\" required>");
-  echo "<br />";
-  print("Manager SSN: <input type=\"text\" name=\"dept_mgr\" required>");
-  echo "<br />";
-  print("<input type=\"submit\" value=\"Submit\">");
+  print("<tr><th>Department Name: </th><th><input type=\"text\" name=\"dept_name\" maxlength=\"20\" required></th></tr>");
+  print("<tr><th>Department Number: </th><th><input type=\"number\" name=\"dept_num\" max=\"9\" required></th></tr>");
+  
+   print("<tr><th><label for=\"dept_mgr\">Manager:</label></th><th><select id=\"dept_mgr\" name=\"dept_mgr\" required>");
+  echo "<option disabled selected value> -- select a manager -- </option>";
+  while($row = $dept_manager_result->fetch_assoc()){
+  echo "<option value=".$row["ssn"].">".$row["fname"]." ".$row["lname"]."</option>";
+  }
+  echo "</select></th></tr>";
+  
+  
+  
+  echo "<br /></table>";
+  print("<input type=\"submit\" name=\"submit2\" value=\"Add Department\">");
   print("</form>");
+   echo "</p></div>";
 }
+$conn->close();
 }
 
 function add_department() {
+
+
+
 
   //var_dump($_POST);
          require('connect.php');
@@ -211,30 +238,28 @@ function add_department() {
          $mgrssn = $_POST['dept_mgr'];
          $stmt->bind_param("sii", $dname, $dnum, $mgrssn);
          if ($stmt->execute()) {
-              echo "ok <a href='index.php'>Back</a>";
+              echo "ok <a href='index.php?page=department'>Back</a>";
          } else 
          {
               echo "Fail";
               echo $stmt->error;
          }
          mysqli_close($conn);
-		 department_page;
+		 department_page();
 }
 
 
 function projects_page() {
 	
 	
-	echo "<div class='container'><h2>Employee Database</h2><table id='example' class='table table-striped table-inverse table-bordered table-hover' cellspacing='0' width='100%'>";
-	echo "<tr><th>Firstn</th><th>Lastn</th><th>Product Name</th></tr><tbody>";
+	echo "<div class='container'><h2>Projects</h2><table id='example' class='table table-striped table-inverse table-bordered table-hover' cellspacing='0' width='100%'>";
+	echo "<thead><tr><th>First</th><th>Last</th><th>Product Name</th></tr></thead><tbody>";
 
 
 require('connect.php');
 
 $sql = "SELECT * FROM works_on LEFT JOIN employee ON employee.ssn = works_on.essn LEFT JOIN project ON project.pnumber = works_on.pno";
 $result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
 	while($row = $result->fetch_assoc()){
 	 echo "<tr>";
 	 echo "<td>".$row["fname"]."</td>";
@@ -242,13 +267,14 @@ if ($result->num_rows > 0) {
 	 echo "<td>".$row["pname"]."</td>";
     
 	 echo "</tr>";
-	}
-} else {
-	echo "0 results";
+
 }
-$conn->close();
 
 echo "</tbody></table></div>";
+
+$conn->close();
+
+
 //$_GET["name"] index?page=ssn
 
 }
@@ -256,14 +282,7 @@ echo "</tbody></table></div>";
 ?>
 </body>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/dataTables.bootstrap4.min.js"></script>
-<script> type="text/javascript"
-  $(document).ready(function() {
-    $('#example').DataTable();
-  });
-</script>
+
 
 
 
